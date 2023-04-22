e5adb295d73ed23765f16b222b06b9c075366d0d
andrew.bunn <andrew.bunn@wsu.edu>
2023-04-21 18:12:53 -0700

    Added gridview that is square and centered

--- app/src/main/java/com/example/networkedcrossword/GameActivity.java
+++ app/src/main/java/com/example/networkedcrossword/GameActivity.java
@@ -1,8 +1,14 @@
 package com.example.networkedcrossword;
 
+import static java.lang.Math.min;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -10,5 +16,21 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        View boardView = findViewById(R.id.gameBoardView);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        int size = min(width, height);
+        int offset = 20;
+
+        System.out.println(size);
+        boardView.setLayoutParams(new ViewGroup.LayoutParams(size-offset, size-offset));
+
+        // Center the boardView
+        boardView.setY((height - size) / 2);
+        boardView.setX(offset/2);
     }
 }
