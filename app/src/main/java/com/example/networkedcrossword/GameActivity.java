659fa5accad36afdf269e49b6efaf049cee0231d
andrew.bunn <andrew.bunn@wsu.edu>
2023-04-21 21:02:05 -0700

    Moved the prompt window and added the game board back

--- app/src/main/java/com/example/networkedcrossword/GameActivity.java
+++ app/src/main/java/com/example/networkedcrossword/GameActivity.java
@@ -8,11 +8,8 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.GridLayout;
 import android.widget.GridView;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -24,37 +21,74 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        View boardView = findViewById(R.id.gameBoardView);
+        View promptView = findViewById(R.id.promptView);
+        View gameBoardView = findViewById(R.id.gameBoard);
 
+        // Get the naviation bar size
+        int offset = 20;
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        int heightNavigationBar = getResources().getDimensionPixelSize(resourceId) + offset;
+        System.out.println("NAV BAR: " + heightNavigationBar);
+
+        // Get the screen size
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        int height = displayMetrics.heightPixels;
-        int width = displayMetrics.widthPixels;
 
-        int size = min(width, height);
-        int offset = 20;
+        int windowHeight = displayMetrics.heightPixels;
+        int windowWidth = displayMetrics.widthPixels;
 
-        System.out.println(size);
-        boardView.setLayoutParams(new ViewGroup.LayoutParams(size-offset, size-offset));
+        int size = min(windowWidth, windowHeight);
+        int sizeUpdated = size - offset;
+        int heightSizeUpdated = size/3 - offset;
 
-        // Center the boardView
-        boardView.setY((height - size) / 2);
-        boardView.setX(offset/2);
 
-        gridView = (GridView) findViewById(R.id.gameBoardView);
+
+
+        // set the size of the prompt view
+        promptView.setLayoutParams(new ViewGroup.LayoutParams(sizeUpdated, heightSizeUpdated));
+
+        // Center the promptView at the bottom of the screen
+        int promptHeight = windowHeight - heightSizeUpdated - heightNavigationBar;
+        promptView.setY(promptHeight);
+        promptView.setX(offset/2);
+
+
+        // set the size of the game board view
+        gameBoardView.setLayoutParams(new ViewGroup.LayoutParams(sizeUpdated, sizeUpdated));
+
+        // Center the gameBoardView between the top of the screen and the top of the promptView
+        gameBoardView.setY(windowHeight - heightNavigationBar - size - heightSizeUpdated);
+        gameBoardView.setX(offset/2);
+
+
+        // Get the dictionary items and display it
+        gridView = (GridView) findViewById(R.id.promptView);
         Dict dict = new Dict(6, 9, this.getApplicationContext());
+
         try {
             dict.build();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
         Set<String> word_set = dict.get_words();
         String[] word_list = new String[word_set.size()];
         HashMap<String, String> map = dict.get_dict();
+
         int count = 0;
         for(String word:word_set) {
             word_list[count++] = count + " _____" + map.get(word) + "Word length is " + word.length();
         }
+
+
+
+
+
+
+
+
+
+
 
 //        for (int i = 0; i < word.length; i++) {
 //            for (int j = 0; i < word.length; j++) {
